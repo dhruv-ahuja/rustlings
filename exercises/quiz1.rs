0ef6af288597d8c7d7d5765436c95bfbbc55ed40
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2023-05-20 18:31:59 +0530

    solve function, if, primitive_types and vec exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    let cost: u32;
+    if amount > 40 {
+        cost = 1
+    } else {
+        cost = 2
+    }
+    amount * cost
+}
 
 // Don't modify this function!
 #[test]
