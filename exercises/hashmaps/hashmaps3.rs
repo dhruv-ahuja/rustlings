714fd26ac85545476b2907267966e80e419ec22d
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2023-05-23 08:09:54 +0530

    complete quiz 2

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -36,24 +34,34 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
 
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
-        // * we have to reuse the Team structs across the loop
-        let mut team_one: Team;
-        let mut team_two: Team;
         if scores.contains_key(&team_1_name) {
-            team_one = scores.key(&team_1_name);
+            let team_one = scores.get_mut(&team_1_name).unwrap();
+
             team_one.goals_scored += team_1_score;
             team_one.goals_conceded += team_2_score;
         } else {
-            team_one = Team {
-                name: team_1_name,
+            let new_team_one = Team {
+                name: team_1_name.clone(),
                 goals_scored: team_1_score,
                 goals_conceded: team_2_score,
-            }
+            };
+
+            scores.insert(team_1_name, new_team_one);
+        }
+
+        if scores.contains_key(&team_2_name) {
+            let team_two = scores.get_mut(&team_2_name).unwrap();
+
+            team_two.goals_scored += team_2_score;
+            team_two.goals_conceded += team_1_score;
+        } else {
+            let new_team_two = Team {
+                name: team_2_name.clone(),
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            };
+
+            scores.insert(team_2_name, new_team_two);
         }
     }
     scores
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // No hints this time!
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,21 +27,26 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = vec![];
+
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            let output_string: String;
+            match command {
+                Command::Uppercase => output_string = string.to_uppercase(),
+                Command::Trim => output_string = string.trim().to_string(),
+                Command::Append(n) => output_string = format!("{}{}", string, "bar".repeat(*n)),
+            }
+            output.push(output_string)
         }
+
         output
     }
 }
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
